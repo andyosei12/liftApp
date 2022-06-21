cabcd91b37ee80dddb5c28d9bca669153494ec85
Drew Osei <andrewowusu21@yahoo.com>
2022-06-21 13:53:15 +0000

    add signup component (#1)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Signup from "../components/Signup";
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.register}>
       <Head>
-        <title>Lift App</title>
-        <meta name="description" content="Be inspired by orhers" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>LiftApp | Register</title>
       </Head>
-
-      <main className={styles.main}>
-        <p>Beginning of something awesome</p>
-      </main>
+      <section className={styles["register-container"]}>
+        <h1 className="mb-large logo">LiftApp</h1>
+        <Signup />
+        <h2>
+          Already have an account?
+          <span> Log in</span>
+        </h2>
+      </section>
+      <p>About section</p>
     </div>
   );
 };
